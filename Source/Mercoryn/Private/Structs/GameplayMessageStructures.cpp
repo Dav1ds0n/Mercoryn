f75ef46f410fc2ab5494d985b3ea3df5fd38f3fb
David Backes <davidbackes@hotmail.de>
2026-04-28 18:13:07 +0200

    Add GameplayMessageStructure for default message sending Add BasePawn > On Mouse over event handling

---
+++ Source/Mercoryn/Private/Structs/GameplayMessageStructures.cpp
@@ -0,0 +1,12 @@
+// Copyright by David Backes
+
+
+#include "Structs/GameplayMessageStructures.h"
+
+GameplayMessageStructures::GameplayMessageStructures()
+{
+}
+
+GameplayMessageStructures::~GameplayMessageStructures()
+{
+}
---
+++ Source/Mercoryn/Public/Structs/GameplayMessageStructures.h
@@ -0,0 +1,15 @@
+// Copyright by David Backes
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MERCORYN_API GameplayMessageStructures
+{
+public:
+	GameplayMessageStructures();
+	~GameplayMessageStructures();
+};
